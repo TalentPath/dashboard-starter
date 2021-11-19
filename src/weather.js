92dd97693115f6aed30006efd0647c577d896fe7
Nathan Daniels <nathandaniels@Nathans-MacBook-Pro.local>
2021-11-19 16:15:48 -0600

    Fixed .gitignore

--- src/weather.js
+++ src/weather.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 import days from './days.js';
+
 
 class Weather {
     constructor(city = 'juneau') {
         this.city = city;
-        this.apiKey = '6ba7ddab25b8409c97d164306211811';
-        this.apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=6ba7ddab25b8409c97d164306211811&q=${this.city}&days=5&aqi=no`;
+        this.apiKey = process.env.WEATHER_API_KEY;
+        this.apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${this.apiKey}&q=${this.city}&days=5&aqi=no`;
         this.forecast = document.getElementById('forecast');
         this.weatherInterval = null;
         this.weatherInfo = null;
@@ -44,8 +46,8 @@ class Weather {
         })
         currentImage.setAttribute('src', day.day.condition.icon);
         currentDate.innerHTML = index === 0 ? 'Today' : days[new Date(day.date).getUTCDay()];
-        currentTemp.innerHTML = `${Math.floor(day.day.maxtemp_f)}&deg;`;
-        currentWind.innerHTML = `${Math.floor(day.day.maxwind_mph)} mph`;
+        currentTemp.innerHTML = `${Math.floor(day.day.maxtemp_f)}&deg; F`;
+        currentWind.innerHTML = `Wind: ${Math.floor(day.day.maxwind_mph)} mph`;
         current.appendChild(expand);
         current.appendChild(currentImage);
         current.appendChild(currentDate);
@@ -75,8 +77,8 @@ class Weather {
 
             currentImage.setAttribute('src', day.day.condition.icon);
             currentDate.innerHTML = i === 0 ? 'Today' : days[new Date(day.date).getUTCDay()];
-            currentTemp.innerHTML = `${Math.floor(day.day.maxtemp_f)}&deg;`;
-            currentWind.innerHTML = `${Math.floor(day.day.maxwind_mph)} mph`;
+            currentTemp.innerHTML = `${Math.floor(day.day.maxtemp_f)}&deg; F`;
+            currentWind.innerHTML = `Wind: ${Math.floor(day.day.maxwind_mph)} mph`;
             current.appendChild(currentImage);
             current.appendChild(currentDate);
             current.appendChild(currentTemp);
