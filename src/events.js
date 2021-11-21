eead49007b62f6960f0670e92b049daf1ddc9199
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-21 11:38:06 -0600

    Added date filter functionality to event.js

--- src/events.js
+++ src/events.js
@@ -2,17 +2,33 @@ class Events {
     constructor() {
         this.eventId = 'MjQ1MTgwODh8MTYzNzI2NTk4OS4wODk3ODg';
         this.clientSecret = '8fd98486b0b4659cd1af37d374d7830c1e0bd7f90f615cad937ff1210fa893a1';
-        this.eventUrl = `https://api.seatgeek.com/2/events?client_id=${this.eventId}&client_secret=${this.clientSecret}&lat=29.76&lon=-95.36&range=50mi&datetime_local.gte=2021-11-21`
+        this.eventUrl = `https://api.seatgeek.com/2/events?client_id=${this.eventId}&client_secret=${this.clientSecret}&lat=29.76&lon=-95.36&range=50mi`
         this.events = null;
         this.eventContainer = document.getElementById('events');
     }
 
-    render() {
-        fetch(this.eventUrl)
+    getEvents(url) {
+        this.eventContainer.innerHTML = '';
+        fetch(url)
             .then((response) => response.json())
             .then((response) => {
                 console.log(response);
                 this.events = response.events;
+                const filter = document.getElementById('filter');
+                const dateFilter = document.getElementById('date-filter');
+
+                filter.addEventListener('click', (event) => {
+                    dateFilter.style.visibility = 'visible';
+                })
+                dateFilter.addEventListener('change', (event) => {
+                    const filteredUrl = this.eventUrl + `&datetime_local.gte=${event.target.value}T00:00:00&datetime_local.lte=${event.target.value}T23:59:59`
+                    this.getEvents(filteredUrl);
+                })
+                dateFilter.addEventListener('keydown', (event) => {
+                    event.preventDefault();
+                    
+                    return false;
+                })
 
                 this.events.forEach(event => {
                     const current = document.createElement('div');
@@ -22,6 +38,10 @@ class Events {
                     this.eventContainer.appendChild(current);
                 })
             })
+    }
+    
+    render() {
+        this.getEvents(this.eventUrl)
     }
 }
 
