67cd78f23602f41efafa0cfcdb37d07faee5876d
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-21 12:29:33 -0600

    Created restaurants.js and connected restaurant API

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 // import Weather from './src/weather.js';
-import Events from './src/events.js';
+// import Events from './src/events.js';
+import Restaurants from "./src/restaurants.js";
 
-// import Countdown from './src/countdown.js'
-// import Giphy from './src/giphy.js'
 
-// let countdown = new Countdown(1,2);
-// countdown.render();
-
-// let gifpane = new Giphy();
-// gifpane.render();
 
 // const weather = new Weather();
 // weather.render();
 
-const events = new Events();
-events.render();
+// const events = new Events();
+// events.render();
+
+const restaurants = new Restaurants();
+restaurants.render();
