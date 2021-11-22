8d2cd8d652252cdf0a01e7ba7d238c53eca83ed1
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-22 16:45:24 -0600

    Restaurant results are now scrollable

--- src/restaurants.js
+++ src/restaurants.js
@@ -4,15 +4,12 @@ class Restaurants {
         this.lon = lon;
         this.restaurantContainer = document.getElementById('restaurants');
         this.restaurantKey = '44b4e42d38793b31f55325a775609ffe';
-        this.restaurantUrl = `https://api.documenu.com/v2/restaurants/search/geo?key=${this.restaurantKey}&lat=${this.lat}&lon=${this.lon}&distance=20`;
+        this.size = 100;
+        this.restaurantUrl = `https://api.documenu.com/v2/restaurants/search/geo?key=${this.restaurantKey}&lat=${this.lat}&lon=${this.lon}&distance=20&size=${this.size}`;
         this.restaurants = null;
-        this.page = 1;
+        this.index = 0;
     }
 
-    setLatLon(lat, lon) {
-        this.lat = lat;
-        this.lon = lon;
-    }
 
     getRestaurants(url = this.restaurantUrl) {
         fetch(url) 
@@ -26,10 +23,12 @@ class Restaurants {
 
     displayResults() {
         this.restaurantContainer.innerHTML = '';
-        this.restaurants.forEach(restaurant => {
-            const restaurantDiv = document.createElement('div');
-            restaurantDiv.innerHTML = `<p>${restaurant.restaurant_name} <span>${restaurant.price_range}</span></p><p>${restaurant.cuisines.join(', ')}</p>`;
-            this.restaurantContainer.appendChild(restaurantDiv);
+        this.restaurants.forEach((restaurant, i) => {
+            if (i >= this.index && i < this.index + 5) {
+                const restaurantDiv = document.createElement('div');
+                restaurantDiv.innerHTML = `<p>${restaurant.restaurant_name} <span>${restaurant.price_range}</span></p><p>${restaurant.cuisines.join(', ')}</p>`;
+                this.restaurantContainer.appendChild(restaurantDiv);
+            }
         })
     }
 
@@ -38,6 +37,8 @@ class Restaurants {
         this.getRestaurants(this.restaurantUrl);
         const restaurantFilter = document.getElementById('restaurant-filter');
         const cuisine = document.getElementById('cuisine');
+        const forward = document.getElementById('forward');
+        const back = document.getElementById('back');
         restaurantFilter.addEventListener('click', (event) => {
             cuisine.style.visibility === 'visible' ? cuisine.style.visibility = 'hidden' : cuisine.style.visibility = 'visible';
         })
@@ -47,6 +48,26 @@ class Restaurants {
             this.restaurantContainer.innerHTML = '';
             this.getRestaurants(filteredUrl);
             cuisine.style.visibility = 'hidden';
+        })
+        forward.addEventListener('click', (event) => {
+            if (this.index === 0) {
+                back.style.display = 'contents';
+            }
+            this.index += 5;
+            if (this.index + 5 >= this.restaurants.length) {
+                forward.style.display = 'none';
+            }
+            this.displayResults();
+        })
+        back.addEventListener('click', (event) => {
+            if (this.index + 5 >= this.restaurants.length) {
+                forward.style.display = 'contents';
+            }
+            this.index -= 5;
+            if (this.index === 0) {
+                back.style.display = 'none';
+            }
+            this.displayResults();
         })
     }
 }
